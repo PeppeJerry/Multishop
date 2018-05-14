7c24e79a1605a49402091c95d44b37eb0fa0f852
PeppeJerry <giuseppe119999@live.it>
2018-05-14 18:50:04 +0200

    Dividing javascript

--- Multishop/website/assets/js/configuration.js
+++ Multishop/website/assets/js/configuration.js
@@ -7,12 +7,4 @@ $(document).ready(function() {
 			e.preventDefault();
 		}
     });
-	
-	//Just Pressing A to Z, a to z, 0 to 9
-	$(".chars").keypress(function(e){
-		var c = String.fromCharCode(e.which);
-		if(!c.match(/[A-Za-z0-9]/g))
-			e.preventDefault();
-	});
-	
 });
