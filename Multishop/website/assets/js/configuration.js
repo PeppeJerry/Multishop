383edc60eef7498d5f189436b90dd540f03f3102
PeppeJerry <giuseppe119999@live.it>
2018-05-14 18:52:14 +0200

    Dividing DOM in different path

--- Multishop/website/assets/js/configuration.js
+++ Multishop/website/assets/js/configuration.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 	
-	//Confirm of the name
+	//Confirm of the database name
+	
 	$("#settings").submit(function(e){
 		var db = document.getElementById("database").value;
 		if(!confirm("Are you sure to use [ "+db+" ] as database name?")){
 			e.preventDefault();
 		}
     });
+	
 });
--- Multishop/website/assets/js/support_function.js
+++ Multishop/website/assets/js/support_function.js
@@ -1,9 +1,11 @@
-//Just Pressing A to Z, a to z, 0 to 9
-$(".chars").keypress(function(e){
+$(document).ready(function() {
+	
+	//Just Pressing A to Z, a to z, 0 to 9
+	
+	$(".chars").keypress(function(e){
 		var c = String.fromCharCode(e.which);
 		if(!c.match(/[A-Za-z0-9]/g))
 			e.preventDefault();
-});
-
-
+	});
 	
+});
